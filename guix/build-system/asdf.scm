f31d0812b32777f61856d73b965ffc8436351f2b
Nicolas Graves <ngraves@ngraves.fr>
2024-01-08 09:02:39 +0100

    build-system: asdf: Redefine asdf-build.
    * guix/build-system/asdf.scm
    (asdf-build): Monadic procedure returns a gexp instead of a derivation.
    
    Change-Id: Ifdd57c4e5279d110ee7c670090b3ae4089703659

--- guix/build-system/asdf.scm
+++ guix/build-system/asdf.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2019, 2020, 2021, 2022 Guillaume Le Vaillant <glv@posteo.net>
 ;;; Copyright © 2021 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2022 Pierre Neidhardt <mail@ambrevar.xyz>
+;;; Copyright © 2024 Nicolas Graves <ngraves@ngraves.fr>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -318,11 +319,8 @@ set up using CL source package conventions."
                                                   search-paths))
                           #:inputs #$(input-tuples->gexp inputs))))))
 
-    (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
-                                                    system #:graft? #f)))
-      (gexp->derivation name builder
-                        #:system system
-                        #:guile-for-build guile))))
+    (mbegin %store-monad
+      (return builder))))
 
 (define asdf-build-system/sbcl
   (build-system
