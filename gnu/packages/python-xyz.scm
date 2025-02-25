94b6a635afff8b349339257294477162c655e0f7
Nicolas Graves <ngraves@ngraves.fr>
2025-02-25 11:09:28 +0100

    build-system/pyproject: Remove python-pip native-input.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -30936,7 +30936,6 @@ By default it uses the open Python vulnerability database Safety DB.")
                   texlive-lm
                   texlive-regexpatch
                   texlive-xcolor))
-           python-pip
            python-poetry-core
            python-setuptools))
     (inputs (list pandoc python-pandocfilters))
