790c30ea449da3b5a9f7c94ce9a009ab80364bc9
Hilton Chain <hako@ultrarare.space>
2025-09-26 00:08:30 +0800

    system: %default-bash-profile: Remove search path setup already handled by /etc/profile.
    * gnu/system/shadow.scm (%default-bash-profile): Remove duplicated exports.
    
    Change-Id: I32fb6da902a4a4daa7545ce4679f8da7787e77b8

--- gnu/system/shadow.scm
+++ gnu/system/shadow.scm
@@ -181,16 +181,6 @@ if [ -f ~/.profile ]; then . ~/.profile; fi
 
 # Honor per-interactive-shell startup file
 if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
-
-# Merge search-paths from multiple profiles, the order matters.
-eval \"$(guix package --search-paths \\
--p $HOME/.config/guix/current \\
--p $HOME/.guix-home/profile \\
--p $HOME/.guix-profile \\
--p /run/current-system/profile)\"
-
-# Prepend setuid programs.
-export PATH=/run/setuid-programs:$PATH
 "))
 
 (define %default-zprofile
