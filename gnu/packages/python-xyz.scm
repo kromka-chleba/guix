d89c360134c9ee2f681a021af5a7e246b88a50eb
John Kehayias <john@guixotic.coop>
2025-09-25 12:23:54 -0400

    gnu: python-fonttools: Fix build.
    * gnu/packages/python-xyz.scm (python-fonttools)[propagated-inputs]: Change
    python-lxml-4.9 to python-lxml.
    
    Change-Id: I5fb32f3cd02dd7d9d537a7464bf0bcc5f0088f93

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -17667,7 +17667,7 @@ from an XML-based format.")
       (propagated-inputs
        (list python-brotli
              python-fs
-             python-lxml-4.9
+             python-lxml
              python-lz4
              python-scipy
              python-unicodedata2
