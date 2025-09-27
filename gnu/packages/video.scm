5929d5b9c9d058ca2c0bc044206dcfb5ec66106a
André Batista <nandre@riseup.net>
2025-09-26 21:01:23 -0300

    gnu: vlc: Build with ffmpeg@6
    * gnu/packages/video.scm (vlc) [inputs]: Replace ffmpeg with ffmpeg-6.
    
    Change-Id: I49ae469dcff11a35122794f10f6b602f264344d6

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -2463,7 +2463,7 @@ media, 2D/3D graphics and ECMAScript.")
            dav1d
            dbus
            eudev
-           ffmpeg
+           ffmpeg-6
            flac
            fontconfig
            freetype
