kromka-chleba/guix

commit 762352c96faa888b2c534f61cc0a48d15a01eda5

gnu: python-setuptools-scm-next: Set to python-setuptools-scm.
Replace all python-setuptools-scm-next occurrences with
python-setuptools-scm.

* gnu/packages/python-build.scm (python-setuptools-scm-next): Set to
  python-setuptools-scm.

Change-Id: I929ca14d24ed05efecded4c23ff02c42d50098c6

gnu/packages/python-build.scm
@@ -707,36 +707,7 @@ system, then @code{flit_core} to build the package.")
 them as the version argument or in a SCM managed file.")
     (license license:expat)))
 
-(define-public python-setuptools-scm-next
-  (package
-    (inherit python-setuptools-scm)
-    (name "python-setuptools-scm")
-    (version "8.1.0")
-    (source (origin
-              (method url-fetch)
-              (uri (pypi-uri "setuptools_scm" version))
-              (sha256
-               (base32 "19y84rzqwb2rd88bjrlafrhfail2bnk6apaig8xskjviayva3pj2"))))
-    (build-system pyproject-build-system)
-    (arguments (list
-                ;; pyproject-build-system will error handle forms such as
-                ;; "module:object", so we set it.
-                #:build-backend "setuptools.build_meta"
-                #:phases
-                #~(modify-phases %standard-phases
-                    (add-before 'build 'setenv
-                      (lambda _
-                        ;; pyproject-build-system ignore backend-path,
-                        ;; and __import__ ignore GUIX_PYTHONPATH, so set
-                        ;; PYTHONPATH.
-                        (setenv "PYTHONPATH"
-                                (string-append
-                                 (getcwd)
-                                 ":"
-                                 (getcwd) "/src:"
-                                 (getenv "GUIX_PYTHONPATH"))))))
-                #:tests? #f))    ;avoid extra dependencies such as pytest
-    (native-inputs (list python-setuptools python-wheel))))
+(define-public python-setuptools-scm-next python-setuptools-scm)
 
 (define-public python-editables
   (package

gnu/packages/astronomy.scm
@@ -1880,7 +1880,7 @@ model-fitting photometry or morphological analyses.")
            python-pytest-doctestplus
            python-pytest-xdist
            python-setuptools
-           python-setuptools-scm-next))
+           python-setuptools-scm))
     (propagated-inputs
      (list python-sunpy))
     (home-page "https://aia.lmsal.com/")
@@ -1977,7 +1977,7 @@ Main features:
            python-pytest-remotedata
            python-pytest-xdist
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-asdf-standard
@@ -2726,7 +2726,7 @@ constraints (i.e., altitude, airmass, moon separation/illumination, etc.)
            python-pytest-astropy-header
            python-pytest-doctestplus
            python-pytest-xdist
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-sgp4
            python-skyfield
            python-threadpoolctl))
@@ -2880,7 +2880,7 @@ celestial-to-terrestrial coordinate transformations.")
            pkg-config
            python-cython-3
            python-extension-helpers
-           python-setuptools-scm-next))
+           python-setuptools-scm))
     (propagated-inputs
      (list python-astropy-iers-data
            python-configobj      ;to replace custom module
@@ -3096,7 +3096,7 @@ Herschel.")
            python-pytest
            python-pytest-cov
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -3367,7 +3367,7 @@ attempting to maintain ISTP compliance
      (list python-pytest-astropy-header
            python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-colorama
@@ -3779,7 +3779,7 @@ Cesium.")
            python-pytest-remotedata
            python-pytest-xdist
            python-setuptools
-           python-setuptools-scm-next))
+           python-setuptools-scm))
     (propagated-inputs
      (list python-aiohttp
            python-asdf
@@ -3880,7 +3880,7 @@ code to be greatly simplified.")
            python-pytest
            python-pytest-remotedata
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astrocut
@@ -3934,7 +3934,7 @@ final image.")
      (list python-astropy-minimal
            python-pytest-astropy
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-numpy
@@ -4309,7 +4309,7 @@ astronomy-specific functionality")
      (list python-pytest
            python-pytest-mpl
            python-setuptools
-           python-setuptools-scm-next))
+           python-setuptools-scm))
     (propagated-inputs
      (list python-astrodendro
            python-astropy
@@ -4367,7 +4367,7 @@ across many files.")
      (list python-objgraph
            python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            xorg-server-for-tests))
     (propagated-inputs
      (list python-astropy
@@ -4493,7 +4493,7 @@ specifically pulsar timing array signals.")
            python-pytest-astropy
            python-pyyaml
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-asdf
@@ -5455,7 +5455,7 @@ on:
            python-pytest-doctestplus
            python-pytest-mpl
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -5553,7 +5553,7 @@ Carlo.")
            python-pytest-xdist
            python-scipy
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-sunpy-minimal
            python-wheel))
     (propagated-inputs
@@ -7052,7 +7052,7 @@ well as ephemerides services
      (list python-cython
            python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-numpy))
@@ -7076,7 +7076,7 @@ well as ephemerides services
      (list python-cython
            python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-numpy
@@ -7542,7 +7542,7 @@ Computing Cluster, HPCC}
     (native-inputs
      (list python-photutils
            python-pytest-astropy
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -8049,7 +8049,7 @@ and CAS statistics), as well as fitting 2D Sérsic profiles.")
            python-pytest-xdist
            python-scipy
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-asdf
@@ -8089,7 +8089,7 @@ implemented in the @acronym{JWST, James Webb Space Telescope} and
      (list python-pytest
            python-pytest-doctestplus
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-asdf
@@ -8346,7 +8346,7 @@ pipelines.")
     (native-inputs
      (list python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-numpy
@@ -8405,7 +8405,7 @@ task}.")
      (list #:tests? #f)) ; no tests in PyPI or git
     (native-inputs
      (list python-setuptools
-           python-setuptools-scm-next))
+           python-setuptools-scm))
     (propagated-inputs
      (list python-astropy
            python-numpy
@@ -8480,7 +8480,7 @@ processing functions: @code{xyxymatch}, @code{geomap}.")
            python-pytest-astropy-header
            python-pytest-doctestplus
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -8588,7 +8588,7 @@ packages for HST.")
      (list nss-certs-for-test
            python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -8636,7 +8636,7 @@ Telescope, HST}).")
            python-pytest-mpl
            python-pytest-xdist
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -8681,7 +8681,7 @@ Telescope, HST}).")
            python-pytest-xdist
            python-reproject
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-streamtracer
            python-sympy))
     (propagated-inputs
@@ -8737,7 +8737,7 @@ and @code{astropy}.")
           (delete 'sanity-check))))
     (native-inputs
      (list python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-corner
@@ -8817,7 +8817,7 @@ SunPy.")
            python-pytest-mpl
            python-pytest-xdist
            python-setuptools
-           python-setuptools-scm-next))
+           python-setuptools-scm))
     (propagated-inputs
      (list python-asdf
            python-asdf-astropy
@@ -8863,7 +8863,7 @@ to the SolarSoft data analysis environment.")
         ((#:tests? _ #t) #f)))
      (native-inputs
       (list python-setuptools
-            python-setuptools-scm-next
+            python-setuptools-scm
             python-wheel))
      (propagated-inputs
       (list python-astropy-minimal
@@ -8927,7 +8927,7 @@ to the SolarSoft data analysis environment.")
            python-pytest-doctestplus
            python-responses
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy
@@ -8972,7 +8972,7 @@ to the SolarSoft data analysis environment.")
            python-pytest-doctestplus
            python-pytest-xdist
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-sunpy-minimal))
     (propagated-inputs
      (list python-astropy
@@ -9079,7 +9079,7 @@ of the old packages.")
      (list python-pytest
            python-scipy
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-astropy

gnu/packages/calendar.scm
@@ -249,7 +249,7 @@ data units.")
            python-packaging
            python-pytest
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-sphinx
            python-sphinxcontrib-newsfeed
            python-wheel))

gnu/packages/python-science.scm
@@ -3371,7 +3371,7 @@ Snakemake and its storage plugins.")
            python-pytest
            python-pytest-cov
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (home-page "https://github.com/pydata/sparse/")
     (synopsis "Library for multi-dimensional sparse arrays")

gnu/packages/python-web.scm
@@ -2675,7 +2675,7 @@ HTTP servers, RESTful APIs, and web services.")
            python-pytest-localserver
            python-pytest-socket
            python-setuptools
-           python-setuptools-scm-next
+           python-setuptools-scm
            python-wheel))
     (propagated-inputs
      (list python-aiofiles

gnu/packages/python-xyz.scm
@@ -40182,7 +40182,7 @@ with one function call.  IceCream makes print debugging a little sweeter.")
          "test/test_ngrams.py")))
     (propagated-inputs (list python-cffi))
     (native-inputs (list python-pytest python-setuptools
-                         python-setuptools-scm-next python-wheel))
+                         python-setuptools-scm python-wheel))
     (home-page "https://github.com/mideind/Icegrams")
     (synopsis "Trigram statistics for Icelandic")
     (description

gnu/packages/version-control.scm
@@ -2833,7 +2833,7 @@ execution of any hook written in any language before every commit.")
     (native-inputs
      (list python-docutils
            ;; The following inputs are only needed to run the tests.
-           python-setuptools python-setuptools-scm-next python-wheel unzip which))
+           python-setuptools python-setuptools-scm python-wheel unzip which))
     (inputs
      (list python-wrapper))
     ;; Find third-party extensions.

commit ed380777fcf8ab797d62f9cae8f9143cf11437d8

gnu: python-setuptools-scm-next: Mark as deprecated.
Replace all python-setuptools-scm-next occurrences
with python-setuptools-scm.

* gnu/packages/python-build.scm (python-setuptools-scm-next):
  Mark as a deprecated public alias.
* gnu/packages/qt.scm (python-sip, python-pyqt-builder) [native-inputs]:
  Replace python-setuptools-next with python-setuptools.

Fixes: 762352c96faa ("gnu: python-setuptools-scm-next: Set to python-setuptools-scm.")
Change-Id: I89479e83d89444f93be4d4c819165c4f12ccf913

gnu/packages/python-build.scm
@@ -686,7 +686,8 @@ system, then @code{flit_core} to build the package.")
 them as the version argument or in a SCM managed file.")
     (license license:expat)))
 
-(define-public python-setuptools-scm-next python-setuptools-scm)
+(define-deprecated/public-alias python-setuptools-scm-next
+  python-setuptools-scm)               ;may be removed after 2025-12-01
 
 (define-public python-editables
   (package

gnu/packages/qt.scm
@@ -4372,7 +4372,7 @@ Python.")
     (arguments
      (list #:tests? #f))        ; No test system found.
     (native-inputs
-     (list python-setuptools python-setuptools-scm-next python-wheel))
+     (list python-setuptools python-setuptools-scm python-wheel))
     (propagated-inputs
      (list python-packaging python-setuptools python-tomli))
     (home-page "https://www.riverbankcomputing.com/software/sip/intro")
@@ -4725,7 +4725,7 @@ set of three modules.")))
          "1g40j5iyad8bw8113rsxxkbkdi7g1lcjj0lb1j7pma442i3fmpsn"))))
     (build-system pyproject-build-system)
     (native-inputs (list python-setuptools
-                         python-setuptools-scm-next
+                         python-setuptools-scm
                          python-wheel))
     (inputs
      (list python-sip))