kromka-chleba/guix

commit bbb955188451c54adb4bedfcb8e259440f3f1bfe

gnu: python-charset-normalizer-3: Set to python-charset-normalizer.
* gnu/packages/python-xyz.scm (python-charset-normalizer-3): Set to
python-charset-normalizer.

Change-Id: I15b2aba11e2892f011095703575c76b463c2f576

gnu/packages/python-xyz.scm
@@ -19742,35 +19742,7 @@ taking a new approach.  All IANA character set names for which the Python core
 library provides codecs are supported.")
     (license license:expat)))
 
-(define-public python-charset-normalizer-3
-  (package
-    (inherit python-charset-normalizer)
-    (name "python-charset-normalizer")
-    (version "3.4.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "charset_normalizer" version))
-       (sha256
-        (base32 "0qqfk84ka3d9hh0yf7n8y0qa0yn08ncdacjjckzix8ybkv5cxbjv"))))
-    (build-system pyproject-build-system)
-    (arguments
-     (substitute-keyword-arguments (package-arguments
-                                    python-charset-normalizer)
-       ((#:phases phases
-         '%standard-phases)
-        #~(modify-phases #$phases
-            ;; https://github.com/jawah/charset_normalizer/issues/625
-            ;; https://github.com/jawah/charset_normalizer/pull/626
-            (add-after 'unpack 'fix-scripts
-              (lambda _
-                (substitute* "pyproject.toml"
-                  (("charset_normalizer:cli.cli_detect")
-                   "charset_normalizer.cli:cli_detect"))))))))
-    (native-inputs
-     (list python-pytest
-           python-setuptools
-           python-wheel))))
+(define-public python-charset-normalizer-3 python-charset-normalizer)
 
 (define-public python-docopt
   (package

commit b7b819c655f7b79e6db15b4fef2b8590ffa8c85a

gnu: python-charset-normalizer-3: Mark as deprecated.
Replace all python-charset-normalizer-3 occurences
with python-charset-normalizer.

* gnu/packages/python-xyz.scm (python-charset-normalizer-3):
  Mark as a deprecated public alias.
* gnu/packages/music.scm (picard)
  [inputs]: Replace python-charset-normalizer-3
  with python-charset-normalizer.
* gnu/packages/python-web.scm (python-translation-finder)
  [propagated-inputs]: Replace python-charset-normalizer-3
  with python-charset-normalizer.

Fixes: bbb955188451 ("gnu: python-charset-normalizer-3: Set to python-charset-normalizer.")
Change-Id: Ib45bfe24efde2f2a6435c0c272d9d14e70e2321e

gnu/packages/music.scm
@@ -4052,7 +4052,7 @@ event-based scripts for scrobbling, notifications, etc.")
      (list gettext-minimal python-dateutil))
     (inputs
      (list chromaprint
-           python-charset-normalizer-3
+           python-charset-normalizer
            python-discid
            python-pyqt
            python-mutagen

gnu/packages/python-web.scm
@@ -8611,7 +8611,7 @@ for HTTP/2 is planned.")
      (list python-pytest
            python-setuptools))
     (propagated-inputs
-     (list python-charset-normalizer-3
+     (list python-charset-normalizer
            python-ruamel.yaml
            python-weblate-language-data))
     (home-page "https://weblate.org/")

gnu/packages/python-xyz.scm
@@ -19853,7 +19853,8 @@ taking a new approach.  All IANA character set names for which the Python core
 library provides codecs are supported.")
     (license license:expat)))
 
-(define-public python-charset-normalizer-3 python-charset-normalizer)
+(define-deprecated/public-alias python-charset-normalizer-3
+  python-charset-normalizer)           ;may be removed after 2025-12-01
 
 (define-public python-docopt
   (package