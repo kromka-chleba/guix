kromka-chleba/guix

commit a34d8e427875e0538a81e4414e92cc41c91fd0cf

gnu: python-flit-core-next: Set to python-flit-core.
* gnu/packages/python-build.scm (python-flit-core-next): Set to
  python-flit-core.

Change-Id: I8ae01214661186b2adb2c7855d54d8c71daeeeb2

gnu/packages/python-build.scm
@@ -663,16 +663,7 @@ specified by PEP 517, @code{flit_core.buildapi}.")
        (delete "python-toml")
        (prepend python-tomli)))))
 
-(define-public python-flit-core-next
-  (package/inherit python-flit-core
-    (name "python-flit-core")
-    (version "3.12.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "flit" version))
-       (sha256
-        (base32 "0h1pxi2hgr95321bgl45l86693zl14l3shj0idsyg4k9v56z700w"))))))
+(define-public python-flit-core-next python-flit-core)
 
 (define-public python-flit-scm
   (package

commit 7d2de9b3419ecb9c050bdd01ad77dc751fc1b3b7

gnu: python-flit-core-next: Mark as deprecated.
Replace all python-flit-core-next occurrences with python-flit-core.

* gnu/packages/python-build.scm (python-flit-core-next):
  Mark as a deprecated public alias.
* gnu/packages/machine-learning.scm (python-pytorch-geometric)
  [propagated-inputs]: Replace python-flit-core-next
  with python-flit-core.
* gnu/packages/pdf.scm (python-pypdf) [propagated-inputs]:
  Replace python-flit-core-next with python-flit-core.
* gnu/packages/python-check.scm (python-stestr) [propagated-inputs]:
  Replace python-flit-core-next with python-flit-core.
* gnu/packages/python-xyz.scm (python-asyncclick) [propagated-inputs]:
  Replace python-flit-core-next with python-flit-core.

Fixes: a34d8e427875 ("gnu: python-flit-core-next: Set to python-flit-core.")
Change-Id: I3a25290f6fd97995b773332addefe33afd016e67

gnu/packages/machine-learning.scm
@@ -4979,7 +4979,7 @@ in the audio domain.")
              python-xxhash))
       (native-inputs
        (list openssl
-             python-flit-core-next
+             python-flit-core
              python-pytest
              python-pytest-cov))
       (home-page "https://pyg.org")

gnu/packages/pdf.scm
@@ -1484,7 +1484,7 @@ converter using the Poppler and Cairo libraries.")
                     " and not "))))
     (native-inputs
      (list python-flit
-           python-flit-core-next
+           python-flit-core
            python-pytest
            python-pytest-socket
            python-pytest-timeout

gnu/packages/python-build.scm
@@ -36,6 +36,7 @@
   #:use-module (guix build-system python)
   #:use-module (guix build-system pyproject)
   #:use-module (guix gexp)
+  #:use-module (guix deprecation)
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages))
@@ -614,7 +615,8 @@ specified by PEP 517, @code{flit_core.buildapi}.")
        (delete "python-toml")
        (prepend python-tomli)))))
 
-(define-public python-flit-core-next python-flit-core)
+(define-deprecated/public-alias python-flit-core-next
+  python-flit-core)                    ;may be removed after 2025-12-01
 
 (define-public python-flit-scm
   (package

gnu/packages/python-check.scm
@@ -3758,7 +3758,7 @@ possibly work.")
     (native-inputs
      (list python-ddt
            python-iso8601
-           python-flit-core-next ;requires >=3.12
+           python-flit-core
            python-setuptools))
     (propagated-inputs
      (list python-cliff-bootstrap

gnu/packages/python-xyz.scm
@@ -411,7 +411,7 @@ APIs to detect, query, and compare them.")
                 (("from click") "from asyncclick")))))))
     (native-inputs
      (list python-anyio
-           python-flit-core-next
+           python-flit-core
            python-pytest
            python-trio))
     (home-page "https://github.com/python-trio/asyncclick")