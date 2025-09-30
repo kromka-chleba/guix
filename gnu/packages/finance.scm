kromka-chleba/guix

commit 9d6ce0d0d45f4cfdf1fddccc999cfc0dde17ddb6

gnu: python-poetry-core-next: Set to python-poetry-core.
* gnu/packages/python-build.scm (python-poetry-core-next): Set to
  python-poetry-core.

Change-Id: Ia69fc596e654b0e015636c8b347a36f449366c72

gnu/packages/python-build.scm
@@ -601,15 +601,7 @@ a light weight, fully compliant, self-contained package allowing PEP 517
 compatible build front-ends to build Poetry managed projects.")
     (license license:expat)))
 
-(define-public python-poetry-core-next
-  (package/inherit python-poetry-core
-    (version "2.1.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "poetry_core" version))
-       (sha256
-        (base32 "015a0802radb2x5wsbywm4fggv4s1srh8zyml5v58xgrxg8bpnzr"))))))
+(define-public python-poetry-core-next python-poetry-core)
 
 ;;; This package exists to bootstrap python-tomli.
 (define-public python-flit-core-bootstrap

commit 93e27c35e56b253d12ee613a47474fdd9dc8fc3e

gnu: python-poetry-core-next: Mark as deprecated.
* gnu/packages/python-build.scm (python-poetry-core-next):
  Mark as a deprecated public alias.
* gnu/packages/finance.scm (python-duniterpy): [native-inputs]:
  Replace python-poetry-core-next with python-poetry-core.

Fixes: 9d6ce0d0d45f ("gnu: python-poetry-core-next: Set to python-poetry-core.")
Change-Id: I12bb7b00db4ad500107ac474333494453b2776dd

gnu/packages/finance.scm
@@ -1617,7 +1617,7 @@ Luhn and family of ISO/IEC 7064 check digit algorithms.")
         (base32 "1ysh9b5lzg053hv4iw3zbn7hid05qssiwmrl8sir8qlk958r8x60"))))
     (build-system pyproject-build-system)
     (native-inputs
-     (list python-poetry-core-next
+     (list python-poetry-core
            python-pytest))
     (propagated-inputs
      (list python-base58

gnu/packages/python-build.scm
@@ -561,7 +561,8 @@ a light weight, fully compliant, self-contained package allowing PEP 517
 compatible build front-ends to build Poetry managed projects.")
     (license license:expat)))
 
-(define-public python-poetry-core-next python-poetry-core)
+(define-deprecated/public-alias python-poetry-core-next
+  python-poetry-core)                  ;may be removed after 2025-12-01
 
 ;;; This package exists to bootstrap python-tomli.
 (define-public python-flit-core-bootstrap