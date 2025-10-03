7ae656a846210f5b631ef0ebc4599e8cab0e9612
Karl Hallsby <karl@hallsby.com>
2025-10-02 20:39:35 -0500

    gnu: Init nut.
    * gnu/packages/power.scm(nut): New variable.
    
    Change-Id: I9e847b0b23b659043b8c7009864a4ab08a2c9dd0

--- gnu/packages/power.scm
+++ gnu/packages/power.scm
@@ -28,12 +28,28 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages avahi)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages documentation)
+  #:use-module (gnu packages docbook)
+  #:use-module (gnu packages engineering)
+  #:use-module (gnu packages freeipmi)
+  #:use-module (gnu packages gd)
   #:use-module (gnu packages libusb)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages man)
+  #:use-module (gnu packages networking)
+  #:use-module (gnu packages nss)
   #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages python-xyz))
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages version-control))
 
 (define-public apcupsd
   (package
@@ -136,3 +152,100 @@ Uninterruptible Power Supply} models.  @command{apcupsd} works with most of
 APC’s Smart-UPS models as well as most simple signalling models such a
 Back-UPS, and BackUPS-Office.")
     (license license:gpl2)))
+
+(define-public nut
+  (package
+    (name "nut")
+    (version "2.8.4")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/networkupstools/nut")
+              (commit (string-append "v" version))))
+       (sha256
+        (base32 "1b64v0bxjn4s6ch3bb1nxycf3qch38jixwnamyhx8rp4bqd70p1r"))
+       (file-name (git-file-name name version))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      #~(list ;; "--with-docs" ;; TODO: Enable docs
+              "--with-python3"
+              ;; Build & install development headers
+              "--with-dev"
+              ;; Add SSL & NSS support
+              "--with-ssl"
+              ;; "--with-nss"
+              "--with-openssl"
+              ;; Build all the drivers we can support
+              "--with-all"
+              "--with-impi" "--with-freeimpi"
+              "--with-cgi"
+              "--with-powerman=no" ; Powerman library not available
+              ;; FIXME: Turn PyNUT on.
+              ;; Attempts to install to "system-wide" Python, which is RO for us
+              "--with-pynut=no"
+              ;; State directories.
+              "--with-statepath=/run"
+              "--with-pidpath=/run"
+              ;; FIXME: Set udev & devd dirs
+              ;; "--with-udev-dir=blah"
+              ;; "--with-devd-dir=blah"
+              ;; Guix does not use systemd, disable those featuers
+              "--with-libsystemd=no")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'bootstrap 'fix-autogen
+            (lambda _
+              ;; Make bootstrap scripts use Guix-encoded values
+              (substitute* "autogen.sh"
+                ;; Use the version string we have in Guix
+                (("^(NUT_VERSION_QUERY=).*$" _ version-var)
+                 (string-append version-var #$version "\n")))
+              (substitute* "configure.ac"
+                ;; Use Guix's version. Remember to add the trailing comma!
+                (("m4_esyscmd_s\\(\\[NUT_VERSION_QUERY=VER50.*")
+                 (string-append #$version ","))
+                ;; Use Guix's URL to the home-page. Remember to add end paren!
+                (("m4_esyscmd_s\\(\\[NUT_VERSION_QUERY=URL.*")
+                 (string-append #$(package-home-page this-package) ")")))
+              ;; Forcibly set CONFIG_SHELL so autogen.sh trusts us that this is
+              ;; the shell to use.
+              (setenv "CONFIG_SHELL" #$(file-append bash-minimal "/bin/bash")))))))
+    (native-inputs
+     (list autoconf automake libtool pkg-config
+           bash-minimal
+           ;; Generate documentation
+           ;; mandoc asciidoc dblatex
+           ;; Perl & Python used in setup scripts.
+           perl python))
+    (inputs
+     (list avahi
+           freeipmi
+           gd
+           i2c-tools
+           libgpiod
+           libmodbus
+           libltdl
+           libusb libusb-compat
+           neon
+           net-snmp
+           ;; nss-certs
+           openssl
+           zlib))
+    (home-page "https://networkupstools.org/")
+    (synopsis "Provides support for power devices")
+    (description "Network UPS Tools (NUT) provides support for Power
+Devices, such as Uninterruptible Power Supplies, Power Distribution Units,
+Automatic Transfer Switches, Power Supply Units and Solar Controllers. NUT
+provides a common protocol and set of tools to monitor and manage such devices,
+and to consistently name equivalent features and data points, across a vast
+range of vendor-specific protocols and connection media types.")
+    (license (list
+              ;; Perl module
+              license:gpl1+
+              ;; Donated Scripts/Installer and Source
+              license:gpl2+
+              ;; Python scripts
+              license:gpl3+))))
