9923c87b401326accc72d47c400562b8158768c5
Hartmut Goebel <h.goebel@crazy-compilers.com>
2025-10-19 18:33:01 +0200

    gnu: Add python-dependency-groups.
    * gnu/packages/python-web.scm (python-dependency-groups): New variable.
    
    Change-Id: I1b284453f9ee0602e71dbfc8c6742cd3802c30f6

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -314,6 +314,27 @@ and JSON.
 @end itemize")
     (license license:expat)))
 
+(define-public python-dependency-groups
+  (package
+    (name "python-dependency-groups")
+    (version "1.3.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "dependency_groups" version))
+       (sha256
+        (base32 "1zgymnk6k984h2wfdpp0vws2ihwqrr9lmxhrij9zs5q5140q61vq"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-packaging python-tomli))
+    (native-inputs (list python-flit-core))
+    (home-page "https://dependency-groups.readthedocs.io/")
+    (synopsis "Tools and library for resolving PEP 735 Dependency Group data")
+    (description
+     "This package provides a library which is able to parse dependency groups (PEP
+735),
+following includes, and provide that data as output.")
+    (license license:expat)))
+
 (define-public python-devpi-common
   (package
     (name "python-devpi-common")
