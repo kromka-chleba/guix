e9acdf8b065a38fa66384bd1e8a9bde19fe0a1a1
Maxim Cournoyer <maxim@guixotic.coop>
2025-10-19 23:54:27 +0900

    gnu: coreutils: Update to 9.8.
    * gnu/packages/base.scm (coreutils): Update to 9.8.
    
    Change-Id: I4010b25f8a8288f149fce010b7d729936986604f

--- gnu/packages/base.scm
+++ gnu/packages/base.scm
@@ -470,14 +470,14 @@ used to apply commands with arbitrarily long arguments.")
 (define-public coreutils
   (package
    (name "coreutils")
-   (version "9.1")
+   (version "9.8")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/coreutils/coreutils-"
                                 version ".tar.xz"))
             (sha256
              (base32
-              "08q4b0w7mwfxbqjs712l6wrwl2ijs7k50kssgbryg9wbsw8g98b1"))))
+              "05fark34w12c9qx2jwjz352pxkd08v8i72m1qahl349chlnzvm76"))))
    (build-system gnu-build-system)
    (inputs `(,acl                                 ;TODO: add SELinux
              ,attr                                ;for xattrs in ls, mv, etc
