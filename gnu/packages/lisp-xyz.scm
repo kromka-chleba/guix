323fff2313119824979e857053875c505d8c3f40
Omar Bassam <omar.bassam88@gmail.com>
2025-10-26 11:52:01 +0200

    gnu: sbcl-colored: Update to 1.0.0-2.54e6184.
    * gnu/packages/lisp-xyz.scm (sbcl-colored): Update to 1.0.0-2.54e6184.
    [source]: Update to use new source url.
    [home-page]: Update to use new homepage url.
    
    Change-Id: I5dff3a942550e00e489ac8bebb824f94417fb572
    Signed-off-by: Omar Bassam <omar.bassam88@gmail.com>

--- gnu/packages/lisp-xyz.scm
+++ gnu/packages/lisp-xyz.scm
@@ -12953,8 +12953,8 @@ collecting values easier.")
   (sbcl-package->ecl-package sbcl-collectors))
 
 (define-public sbcl-colored
-  (let ((commit "bee87efb0b047da0f071f5cf1457997ab5f93feb")
-        (revision "1"))
+  (let ((commit "54e6184da4aca380cc7240c417f1e2a0d76b3a84")
+        (revision "2"))
     (package
       (name "sbcl-colored")
       (version (git-version "1.0.0" revision commit))
@@ -12962,11 +12962,11 @@ collecting values easier.")
        (origin
          (method git-fetch)
          (uri (git-reference
-               (url "https://github.com/Shinmera/colored/")
+               (url "https://codeberg.org/shinmera/colored")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0mpg91r6yfb9xqccd4r8z3hl2qzjhdj6daswb1cinrm8ffxrvy5k"))))
+          (base32 "1n9k8rh2p7kb2ikxpgw8sywaxw0n5m3iky808dwc4hqz04piqmmh"))))
       (build-system asdf-build-system/sbcl)
       (native-inputs
        (list sbcl-parachute))
@@ -12976,7 +12976,7 @@ collecting values easier.")
       (description
        "This is a library for representing and mapping colours between their
 various spaces.")
-      (home-page "https://shinmera.github.io/colored/")
+      (home-page "https://shinmera.com/docs/colored")
       (license license:zlib))))
 
 (define-public cl-colored
