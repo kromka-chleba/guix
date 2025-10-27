4024f97687ed7e63db4b22b65eda33ec7cf3db63
Igorj Gorjaĉev <igor@goryachev.org>
2025-10-27 17:23:05 +0200

    gnu: Add ejabberd-vendor.
    * gnu/packages/beam-apps.scm (ejabberd-vendor): New variable.
    
    Change-Id: I664da461fc09bd9fb4d242f1c52dcb792277094c

--- gnu/packages/beam-apps.scm
+++ gnu/packages/beam-apps.scm
@@ -17,6 +17,13 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages beam-apps)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages erlang)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages serialization)
+  #:use-module (gnu packages tls)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix gexp)
   #:use-module (guix packages)
@@ -24,3 +31,128 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system mix)
   #:use-module (guix build-system rebar))
+
+(define-public ejabberd-vendor
+  (package
+    (name "ejabberd-vendor")
+    (version "25.08")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/processone/ejabberd")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0cr9s37a1v06i3bqb471r4dw1hdv49ph3pwjjrmnm8xkhyplaaly"))))
+    (build-system rebar-build-system)
+    (inputs (list bash-minimal coreutils procps sed))
+    (native-inputs
+     (list autoconf
+           automake
+           erlang-pc
+           erlang-proper
+           erlang-provider-asn1
+           libyaml
+           linux-pam
+           openssl))
+    (arguments
+     (list
+      #:vendorize? #t
+      #:vendor-inputs `(("." . ,(rebar-inputs 'ejabberd)))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'set-environment
+            (lambda _
+              (setenv "HOME" "/tmp")
+              (setenv "CC" "gcc")))
+          (add-after 'unpack 'bootstrap
+            (lambda _
+              (invoke "aclocal" "-I" "m4")
+              (invoke "autoconf" "-f")))
+          (add-after 'bootstrap 'make-various-fixes
+            (lambda* (#:key inputs #:allow-other-keys)
+              (let ((sh (search-input-file inputs "/bin/sh")))
+                ;; Fix shell locations.
+                (substitute* "configure" (("/bin/sh") sh))
+                (substitute* "rebar.config.script"
+                  (("sh -c '") (string-append sh " -c '")))
+                ;; Do not recompile dependences.
+                (substitute* "rebar.config"
+                  (("\\[\\{\"eimp\", \\[\\]\\},") "[]}.\n{nop, ["))
+                ;; Do not include source files into release.
+                (substitute* "rebar.config"
+                  (("\\{include_src, true\\},") "{include_src, false},"))
+                ;; Do not install erl wrapper, we will do it ourselves.
+                (substitute* "rebar.config"
+                  (("\\{copy, \"rel/files/erl\",")
+                   "%{copy, \"rel/files/erl\","))
+                ;; It seems ejabberd still needs jiffy due to p1_acme.
+                (substitute* "rebar.config"
+                  (("\\{if_version_below, \"27\",") "{if_version_below, \"30\","))
+                ;; Unpin pinned dependences.
+                (substitute* "rebar.lock"
+                  ((",1\\}") ",0}"))
+                ;; Set proper paths.
+                (substitute* "vars.config.in"
+                  (("\\{sysconfdir, \".*\"\\}\\.")
+                   "{sysconfdir, \"/etc\"}."))
+                (substitute* "vars.config.in"
+                  (("\\{localstatedir, \".*\"\\}\\.")
+                   "{sysconfdir, \"/var\"}."))
+                (substitute* "vars.config.in"
+                  (("\\{config_dir, \".*\"\\}\\.")
+                   "{config_dir, \"/etc/ejabberd\"}."))
+                (substitute* "vars.config.in"
+                  (("\\{logs_dir, \".*\"\\}\\.")
+                   "{logs_dir, \"/var/log/ejabberd\"}."))
+                (substitute* "vars.config.in"
+                  (("\\{spool_dir, \".*\"\\}\\.")
+                   "{spool_dir, \"/var/lib/ejabberd\"}.")))))
+          (add-after 'make-various-fixes 'configure
+            (lambda _
+              (invoke "./configure"
+                      (string-append "--prefix=" #$output))))
+          (replace 'build
+            (lambda _
+              (invoke "make" "rel")))
+          (replace 'install
+            (lambda _
+              (let ((ejabberd "_build/prod/rel/ejabberd"))
+                (copy-recursively
+                 (string-append ejabberd "/conf")
+                 (string-append ejabberd "/share/doc/ejabberd-"
+                                #$version "/examples"))
+                (for-each
+                 (lambda (rmdir)
+                   (delete-file-recursively
+                    (string-append ejabberd "/" rmdir)))
+                 '("conf" "database" "logs"))
+                (delete-file
+                 (string-append (string-append ejabberd "/ejabberd-"
+                                               #$version ".tar.gz")))
+                (let ((erts (car (find-files ejabberd "erts-.*"
+                                             #:directories? #t))))
+                  (delete-file (string-append erts "/bin/erl"))
+                  (install-file "rel/files/erl"
+                                (string-append erts "/bin")))
+                (chmod (string-append ejabberd
+                                      "/bin/install_upgrade.escript") #o755)
+                (copy-recursively ejabberd #$output))))
+          (add-after 'install 'wrap-program
+            (lambda* (#:key inputs outputs #:allow-other-keys)
+              (let ((out (assoc-ref outputs "out")))
+                (wrap-program (string-append out "/bin/ejabberdctl")
+                  `("PATH" ":" suffix
+                    ,(map (lambda (command)
+                            (dirname
+                             (search-input-file
+                              inputs (string-append "bin/" command))))
+                          (list "date" "dirname" "grep"
+                                "id" "pgrep" "sed"))))))))))
+    (synopsis "Robust, Ubiquitous and Massively Scalable Messaging Platform")
+    (description "This package provides Ejabberd -- Robust, Ubiquitous and
+Massively Scalable Messaging Platform.  It supports XMPP, MQTT and SIP
+protocols.")
+    (home-page "https://www.ejabberd.im")
+    (license license:gpl2+)))
--- gnu/packages/beam-packages.scm
+++ gnu/packages/beam-packages.scm
@@ -35,6 +35,114 @@
 
 (define aaaa-separator 'begin-of-beam-packages)
 
+(define beam-base64url-1.0.1
+  (hexpm-source "base64url" "base64url" "1.0.1"
+                "0p4zf53v86zfpnk3flinjnk6cx9yndsv960386qaj0hsfgaavczr"))
+
+(define beam-cache-tab-1.0.33
+  (hexpm-source "cache_tab" "cache_tab" "1.0.33"
+                "002rqgikbdnzfkzw4n2wi6k03155pcqf4j68w2mjmcjhn2g00n22"))
+
+(define beam-eimp-1.0.26
+  (hexpm-source "eimp" "eimp" "1.0.26"
+                "0k04abnna5vqd0r248car4xkfjc83p5z0iqy4w7w9pxrfa2lwvfr"))
+
+(define beam-epam-1.0.14
+  (hexpm-source "epam" "epam" "1.0.14"
+                "12frsirp8m0ajdb19xi1g86zghhgvld5cgw459n2m9w553kljd1g"))
+
+(define beam-eredis-1.7.1
+  (hexpm-source "eredis" "eredis" "1.7.1"
+                "1h9wihjqs4fmgr5ihqpisf7k99h006dsf71lygp5zmgycv2m8avw"))
+
+(define beam-esip-1.0.59
+  (hexpm-source "esip" "esip" "1.0.59"
+                "1rpvsfm5y932wfra1mvkqhdabikmwqlh65bky52b3h4x6hy2xpqb"))
+
+(define beam-ezlib-1.0.15
+  (hexpm-source "ezlib" "ezlib" "1.0.15"
+                "1arfjvipmfvz52szlsy6gn4s1x25spip6gljwv7za6jj29nbl56x"))
+
+(define beam-fast-tls-1.1.25
+  (hexpm-source "fast_tls" "fast_tls" "1.1.25"
+                "08d894ckv6flwagngk5zwmgrwxz7nmrycsxap010wrqffjsq7qar"))
+
+(define beam-fast-xml-1.1.57
+  (hexpm-source "fast_xml" "fast_xml" "1.1.57"
+                "0fcwj8yifwhr5m5maqa0ifwp7vad05d67ayxsmky9bxcmn84xhzf"))
+
+(define beam-fast-yaml-1.0.39
+  (hexpm-source "fast_yaml" "fast_yaml" "1.0.39"
+                "13d7n1zjgvnkrxjk7riignqssrh952hs5x259vb6k4ibksmvkir4"))
+
+(define beam-idna-6.1.1
+  (hexpm-source "idna" "idna" "6.1.1"
+                "1sjcjibl34sprpf1dgdmzfww24xlyy34lpj7mhcys4j4i6vnwdwj"))
+
+(define beam-jiffy-1.1.2
+  (hexpm-source "jiffy" "jiffy" "1.1.2"
+                "10gkbi48in96bzkv7f2cqw9119krpd40whcsn0yd7fr0lx1bqqdv"))
+
+(define beam-jose-1.11.10
+  (hexpm-source "jose" "jose" "1.11.10"
+                "0576jdjygby37qmzrs8cm5l6n622b0mi3z28j6r4s5xsz1px6v0d"))
+
+(define beam-luerl-1.2.3
+  (hexpm-source "luerl" "luerl" "1.2.3"
+                "1v9svw2ki9dsaqazkgv23dj158pmx5g6lykqsb8q1lnpll69sjqv"))
+
+(define beam-mqtree-1.0.19
+  (hexpm-source "mqtree" "mqtree" "1.0.19"
+                "0g6fz25j942ryc6m6c6iyb9hvs22v3i5l2pq28l8i8a9biqna468"))
+
+(define beam-p1-acme-1.0.28
+  (hexpm-source "p1_acme" "p1_acme" "1.0.28"
+                "08v4shjng4gdq6nffsvckhs9lcj5rcipbs5ghp95z79zvs36js6f"))
+
+(define beam-p1-mysql-1.0.26
+  (hexpm-source "p1_mysql" "p1_mysql" "1.0.26"
+                "1v7xz81wqx2c6ndl9rd3kq0v125209cbz7alrywijiy5ya1q04za"))
+
+(define beam-p1-oauth2-0.6.14
+  (hexpm-source "p1_oauth2" "p1_oauth2" "0.6.14"
+                "13xfk4flaqb3nsxirf3vmy3yv67n6s6xzil7bafjswj39r3srlqz"))
+
+(define beam-p1-pgsql-1.1.35
+  (hexpm-source "p1_pgsql" "p1_pgsql" "1.1.35"
+                "1hjmw82f6k2dpchgdn2i0j0bvi7m6qihcnvrjq36c721di2995g9"))
+
+(define beam-p1-utils-1.0.28
+  (hexpm-source "p1_utils" "p1_utils" "1.0.28"
+                "0cq0gwd4vy51j1qq2c6p1i6nv98agvfjdy0sd6bdj2m4qi5x96y4"))
+
+(define beam-pkix-1.0.10
+  (hexpm-source "pkix" "pkix" "1.0.10"
+                "03jxmjirg98r1zq7b1f3mnwm8pb1iac2iaxi85615jwl63w688g0"))
+
+(define beam-sqlite3-1.1.15
+  (hexpm-source "sqlite3" "sqlite3" "1.1.15"
+                "0mr8kpv8hf4yknx8vbmyakgasrhk64ldsbafvr4svhi26ghs82rw"))
+
+(define beam-stringprep-1.0.33
+  (hexpm-source "stringprep" "stringprep" "1.0.33"
+                "1h4qvajlsfqfg61c9f0rjf3rmha8sahvqiivnc2zd1q8ql5v7y4n"))
+
+(define beam-stun-1.2.21
+  (hexpm-source "stun" "stun" "1.2.21"
+                "1n8j3vf8g2aq7i271lcm5202vzvvif5vz9m9d8528nyhp7pyhzrx"))
+
+(define beam-unicode-util-compat-0.7.1
+  (hexpm-source "unicode_util_compat" "unicode_util_compat" "0.7.1"
+                "0hinn81kwkr3fvxb4vvp6qqnf19f23hd2jkl34v27bp39j2igadk"))
+
+(define beam-xmpp-1.11.1
+  (hexpm-source "xmpp" "xmpp" "1.11.1"
+                "05ipgf53dnz5d7hfz0bvmv1lxs0c85737ni5ak0wxcsaj3gk7jd5"))
+
+(define beam-yconf-1.0.21
+  (hexpm-source "yconf" "yconf" "1.0.21"
+                "0v093p002l0zw8v8g4f3l769fkr0ihv2xk39nj2mv1w6zpqsa965"))
+
 (define cccc-separator 'end-of-beam-packages)
 
 
@@ -49,4 +157,32 @@
 ;;; Rebar inputs.
 ;;;
 
-(define-rebar-inputs lookup-rebar-inputs)
+(define-rebar-inputs lookup-rebar-inputs
+                     (ejabberd =>
+                               (list beam-yconf-1.0.21
+                                     beam-xmpp-1.11.1
+                                     beam-unicode-util-compat-0.7.1
+                                     beam-stun-1.2.21
+                                     beam-stringprep-1.0.33
+                                     beam-sqlite3-1.1.15
+                                     beam-pkix-1.0.10
+                                     beam-p1-utils-1.0.28
+                                     beam-p1-pgsql-1.1.35
+                                     beam-p1-oauth2-0.6.14
+                                     beam-p1-mysql-1.0.26
+                                     beam-p1-acme-1.0.28
+                                     beam-mqtree-1.0.19
+                                     beam-luerl-1.2.3
+                                     beam-jose-1.11.10
+                                     beam-jiffy-1.1.2
+                                     beam-idna-6.1.1
+                                     beam-fast-yaml-1.0.39
+                                     beam-fast-xml-1.1.57
+                                     beam-fast-tls-1.1.25
+                                     beam-ezlib-1.0.15
+                                     beam-esip-1.0.59
+                                     beam-eredis-1.7.1
+                                     beam-epam-1.0.14
+                                     beam-eimp-1.0.26
+                                     beam-cache-tab-1.0.33
+                                     beam-base64url-1.0.1)))
