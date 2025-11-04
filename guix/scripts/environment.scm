kromka-chleba/guix

commit d12c4452a49b355369636de1dfc766b5bad6437b

shell, inferior: Store GC roots under /var/guix/profiles.
Fixes guix/guix#2410.

Until now, ‘guix shell’ and ‘guix time-machine’ would store GC roots under
~/.cache/guix.  However, this directory is unreadable to guix-daemon when it’s
running without root privileges.  This commit changes ‘guix shell’ and ‘guix
time-machine’ so they store GC roots under /var/guix/profiles/per-user/$USER,
in a world-readable directory.

An added benefit is that, in cluster setups, user homes no longer need to be
mounted on the head node for GC to work (assuming ‘guix build -r’ and similar
are not used).

* guix/inferior.scm (%inferior-cache-directory): Change default value to be
under ‘%profile-directory’.
(%legacy-inferior-cache-directory): New variable.
(cached-channel-instance): Add ‘maybe-remove-expired-cache-entries’ call.
* guix/scripts/environment.scm (launch-environment/container)[nesting-mappings]:
Add /inferiors and /profiles sub-directories of ‘%profile-directory’.  Call
‘mkdir-p’ for these two directories.
* guix/scripts/shell.scm (%profile-cache-directory): Change default value to
be under ‘%profile-directory’.
(%legacy-cache-directory): New variable.
(guix-shell): Add call to ‘maybe-remove-expired-cache-entries’.

Change-Id: Ie7d6c16a55b35c7beb18078c967d6fc902bf68d0
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

guix/scripts/environment.scm
@@ -793,11 +793,24 @@ WHILE-LIST."
 
   (define (nesting-mappings)
     ;; Files shared with the host when enabling nesting.
+
+    ;; Make sure these two directories exist so they can be shared.
+    (mkdir-p (string-append %profile-directory "/profiles"))
+    (mkdir-p (string-append %profile-directory "/inferiors"))
+
     (cons* (file-system-mapping
             (source (%store-prefix))
             (target source))
            (file-system-mapping
-            (source (cache-directory))
+            (source (cache-directory))           ;~/.cache/guix/checkouts etc.
+            (target source)
+            (writable? #t))
+           (file-system-mapping                  ;'guix shell' cached GC roots
+            (source (string-append %profile-directory "/profiles"))
+            (target source)
+            (writable? #t))
+           (file-system-mapping           ;'guix time-machine' cached GC roots
+            (source (string-append %profile-directory "/inferiors"))
             (target source)
             (writable? #t))
            (let ((uri (string->uri (%daemon-socket-uri))))

guix/scripts/shell.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2021-2024 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2021-2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2023 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -327,10 +327,13 @@ echo ~a >> ~a
 ;;;
 
 (define %profile-cache-directory
-  ;; Directory where profiles created by 'guix shell' alone (without extra
-  ;; options) are cached.
-  (make-parameter (string-append (cache-directory #:ensure? #f)
-                                 "/profiles")))
+  ;; Directory where profiles (GC roots) created by 'guix shell' are cached.
+  ;; It must be world-readable so the daemon can traverse it.
+  (make-parameter (string-append %profile-directory "/profiles")))
+
+(define %legacy-cache-directory
+  ;; Former cache directory, by default under $HOME/.cache.
+  (string-append (cache-directory #:ensure? #f) "/profiles"))
 
 (define (profile-cache-primary-key)
   "Return the \"primary key\" used when computing keys for the profile cache.
@@ -591,6 +594,13 @@ to make sure your shell does not clobber environment variables."))) )
                (lambda _
                  (maybe-remove-expired-cache-entries
                   (%profile-cache-directory)
+                  cache-entries
+                  #:entry-expiration entry-expiration)
+
+                 ;; Clean the legacy cache directory as well.  Remove this
+                 ;; call once at least one year has passed.
+                 (maybe-remove-expired-cache-entries
+                  %legacy-cache-directory
                   cache-entries
                   #:entry-expiration entry-expiration)))
 

guix/inferior.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2018-2024 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2018-2025 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -860,9 +860,13 @@ failing when GUIX is too old and lacks the 'guix repl' command."
 ;;;
 
 (define %inferior-cache-directory
-  ;; Directory for cached inferiors (GC roots).
-  (make-parameter (string-append (cache-directory #:ensure? #f)
-                                 "/inferiors")))
+  ;; Directory for cached inferiors (GC roots).  It must be world-readable so
+  ;; the daemon can traverse it.
+  (make-parameter (string-append %profile-directory "/inferiors")))
+
+(define %legacy-inferior-cache-directory
+  ;; Former directory for cached inferiors, by default under $HOME/.cache.
+  (string-append (cache-directory #:ensure? #f) "/inferiors"))
 
 (define* (channel-full-commit channel #:key (verify-certificate? #t))
   "Return the commit designated by CHANNEL as quickly as possible.  If
@@ -949,6 +953,14 @@ X.509 host certificate; otherwise, warn about the problem and keep going."
                                       cache-entries
                                       #:entry-expiration
                                       (file-expiration-time ttl))
+
+  ;; Clean the legacy cache directory as well.  Remove this call once at least
+  ;; one year has passed.
+  (maybe-remove-expired-cache-entries %legacy-inferior-cache-directory
+                                      cache-entries
+                                      #:entry-expiration
+                                      (file-expiration-time ttl))
+
 
   (if (file-exists? cached)
       cached

commit 0195b067ad27b55636f304436500be889b9dd2d0

shell: Create cached profile directory once connected to the daemon.
Fixes a regression introduced in d12c4452a49b355369636de1dfc766b5bad6437b,
where ‘guix shell’, on a fresh installation, would attempt to create the
cached profile directory under /var/guix/profiles/per-user/$USER, which only
guix-daemon can do, on the user’s first connection.

* guix/scripts/shell.scm (options-with-caching): Remove ‘mkdir-p’ call for
ROOT’s parent directory.  Add ‘create-gc-root-directory?’ key to the result.
* guix/scripts/environment.scm (guix-environment*): Honor
‘create-gc-root-directory?’ key.

Fixes: guix/guix#126
Reported-by: Maxim Cournoyer <maxim@guixotic.coop>
Change-Id: I782461c80c254467108c6b8d676d269866795746

guix/scripts/environment.scm
@@ -1235,6 +1235,8 @@ command-line option processing with 'parse-command-line'."
                                       (if prof-drv (list prof-drv) '())
                                       (if (derivation? bash) (list bash) '()))))
                 (mwhen gc-root
+                  (mwhen (assoc-ref opts 'create-gc-root-directory?)
+                    (return (mkdir-p (dirname gc-root))))
                   (register-gc-root profile gc-root))
 
                 (mwhen (assoc-ref opts 'check?)

guix/scripts/shell.scm
@@ -262,10 +262,14 @@ cache) or a 'gc-root' key (to add the profile to cache)."
                               opts)))             ;load right away
         (if (and root (not (assq-ref opts 'gc-root)))
             (begin
-              (if stat
-                  (delete-file root)
-                  (mkdir-p (dirname root)))
-              (alist-cons 'gc-root root opts))
+              (when stat
+                (delete-file root))
+              ;; Note: Delay the creation of ROOT's parent directory: a
+              ;; connection must first be made to the daemon so that it
+              ;; creates /var/guix/profiles/per-user/$USER if needed.
+              (alist-cons 'gc-root root
+                          (alist-cons 'create-gc-root-directory? #t
+                                      opts)))
             opts))))
 
 (define (auto-detect-manifest opts)