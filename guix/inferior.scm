kromka-chleba/guix

commit 529dce6e98c22f00052df231414bca4f86e02094

inferior: Caching works by checking the mtime, not the atime.
Mounting file systems with ‘noatime’ is common.  Thus, avoid relying on the
atime.

* guix/inferior.scm (cached-channel-instance): Pass ‘stat:mtime’ to
‘file-expiration-time’.  When ‘cached’ exists, call ‘utime’ to update its
mtime and atime.

Change-Id: Ib9bff45ed402260fdead3c23f09b431fa369e741

guix/inferior.scm
@@ -952,18 +952,21 @@ X.509 host certificate; otherwise, warn about the problem and keep going."
   (maybe-remove-expired-cache-entries cache-directory
                                       cache-entries
                                       #:entry-expiration
-                                      (file-expiration-time ttl))
+                                      (file-expiration-time ttl stat:mtime))
 
   ;; Clean the legacy cache directory as well.  Remove this call once at least
   ;; one year has passed.
   (maybe-remove-expired-cache-entries %legacy-inferior-cache-directory
                                       cache-entries
                                       #:entry-expiration
-                                      (file-expiration-time ttl))
+                                      (file-expiration-time ttl stat:mtime))
 
 
   (if (file-exists? (cached commits))
-      (cached commits)
+      (let ((now (current-time)))
+        ;; Update the mtime on CACHED to reflect usage.
+        (utime cached now now 0 0 AT_SYMLINK_NOFOLLOW)
+        cached)
       (run-with-store store
         (mlet* %store-monad ((instances
                               -> (latest-channel-instances store channels

commit d4c873ca4ac3e6b402d803d448f8da854caa66f2

inferior: fix cached-channel-instance utime call and return value
* inferior.scm (cached-channel-instance):
  Use (cached commit) let-bound to cached-directory instead as value
  to utime and return-value.
  This fixes a bug introduced in 529dce6e98c22f00052df231414bca4f86e02094 where
  the utime procedure would just get a procedure rather than the actual file.
  fixes: guix/guix#4042

Change-Id: Ibe7fb78fa68292d70e3bbe4ab9234757510cc539

guix/inferior.scm
@@ -963,10 +963,11 @@ X.509 host certificate; otherwise, warn about the problem and keep going."
 
 
   (if (file-exists? (cached commits))
-      (let ((now (current-time)))
+      (let ((now (current-time))
+            (cached-directory (cached commits)))
         ;; Update the mtime on CACHED to reflect usage.
-        (utime cached now now 0 0 AT_SYMLINK_NOFOLLOW)
-        cached)
+        (utime cached-directory now now 0 0 AT_SYMLINK_NOFOLLOW)
+        cached-directory)
       (run-with-store store
         (mlet* %store-monad ((instances
                               -> (latest-channel-instances store channels