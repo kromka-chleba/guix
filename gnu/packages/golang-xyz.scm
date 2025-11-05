9d694686e287344fd7ad27b22724a54fb466bd4b
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-11-05 10:39:39 +0100

    gnu: Add go-github-com-danielgtaylor-mexpr.
    * gnu/packages/golang-xyz.scm (go-github-com-danielgtaylor-mexpr): New variable.
    
    Change-Id: I8dea2187882c0adaec7c9974a64c9f6ea78b45ea

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -5474,6 +5474,61 @@ credentials as well as Go structures for convenient access to the credential
 data.")
     (license license:expat)))
 
+(define-public go-github-com-danielgtaylor-mexpr
+  (package
+    (name "go-github-com-danielgtaylor-mexpr")
+    (version "1.9.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/danielgtaylor/mexpr")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1kiya25njhy5k9jpk79dz07bwzl33x19mjl4h1sjmdh98aw41z3r"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/danielgtaylor/mexpr"
+      #:tests? #f)) ;tests broken. reenable after update
+    (home-page "https://github.com/danielgtaylor/mexpr")
+    (synopsis "MicroExpr")
+    (description "Package mexpr provides a simple expression parser.")
+    (license license:expat)))
+
+(define-public go-github-com-danielgtaylor-shorthand-v2
+  (package
+    (name "go-github-com-danielgtaylor-shorthand-v2")
+    (version "2.2.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/danielgtaylor/shorthand")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "17s4jjzfz10lavwnir8cd6nwsf15hrm38y87f7w2pi9hv40x7vn9"))
+       (modules '((guix build utils)))
+       (snippet #~(begin
+                    ;; Submodules with their own go.mod files and packaged separately:
+                    (delete-file-recursively "cmd")))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/danielgtaylor/shorthand/v2"
+      #:tests? #f)) ;tests broken. reenable after update
+    (propagated-inputs (list go-github-com-stretchr-testify
+                             go-github-com-fxamacker-cbor-v2
+                             go-github-com-danielgtaylor-mexpr))
+    (home-page "https://github.com/danielgtaylor/shorthand")
+    (synopsis "Structured Data Shorthand Syntax")
+    (description
+     "Shorthand is a superset and friendlier variant of JSON designed with several
+use-cases in mind:.")
+    (license license:expat)))
+
 (define-public go-github-com-dannav-hhmmss
   (package
     (name "go-github-com-dannav-hhmmss")
