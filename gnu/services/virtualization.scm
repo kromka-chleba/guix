kromka-chleba/guix

commit bab6434f5855b92631615fdd8a2d2a225da28359

services: hurd-vm: ‘image’ field has to be an <image> record.
* gnu/services/virtualization.scm (<hurd-vm-configuration>)[image]:
Document as being an <image> record.
(hurd-vm-disk-image): Remove call to ‘system-image’.
(hurd-vm-shepherd-service): Add call to ‘system-image’.
* gnu/tests/virtualization.scm (hurd-vm-disk-image-raw): Remove call to
‘system-image’.
* doc/guix.texi (Virtualization Services): Adjust accordingly.

doc/guix.texi
@@ -35770,8 +35770,8 @@ permissive OpenSSH secure shell daemon listening on port 2222
 The QEMU package to use.
 
 @item @code{image} (default: @var{hurd-vm-disk-image})
-The procedure used to build the disk-image built from this
-configuration.
+The image object representing the disk image of this virtual machine
+(@pxref{System Images}).
 
 @item @code{disk-size} (default: @code{'guess})
 The size of the disk image.

gnu/services/virtualization.scm
@@ -1101,7 +1101,7 @@ that will be listening to receive secret keys on port 1004, TCP."
                (default %hurd-vm-operating-system))
   (qemu        hurd-vm-configuration-qemu               ;file-like
                (default qemu-minimal))
-  (image       hurd-vm-configuration-image              ;string
+  (image       hurd-vm-configuration-image              ;<image>
                (thunked)
                (default (hurd-vm-disk-image this-record)))
   (disk-size   hurd-vm-configuration-disk-size          ;number or 'guess
@@ -1126,9 +1126,8 @@ is added to the OS specified in CONFIG."
          (disk-size (hurd-vm-configuration-disk-size config))
          (type      (lookup-image-type-by-name 'hurd-qcow2))
          (os->image (image-type-constructor type)))
-    (system-image
-     (image (inherit (os->image os))
-            (size disk-size)))))
+    (image (inherit (os->image os))
+           (size disk-size))))
 
 (define (hurd-vm-port config base)
   "Return the forwarded vm port for this childhurd config."
@@ -1170,7 +1169,7 @@ is added to the OS specified in CONFIG."
                       "-m" (number->string #$memory-size)
                       #$@net-options
                       #$@options
-                      "--hda" #+image
+                      "--hda" #+(system-image image)
 
                       ;; Cause the service to be respawned if the guest
                       ;; reboots (it can reboot for instance if it did not

gnu/tests/virtualization.scm
@@ -230,12 +230,11 @@
   (let ((os ((@@ (gnu services virtualization) secret-service-operating-system)
              (hurd-vm-configuration-os config)))
         (disk-size (hurd-vm-configuration-disk-size config)))
-    (system-image
-     (image
-      (inherit hurd-disk-image)
-      (format 'disk-image)
-      (size disk-size)
-      (operating-system os)))))
+    (image
+     (inherit hurd-disk-image)
+     (format 'disk-image)
+     (size disk-size)
+     (operating-system os))))
 
 (define %childhurd-os
   (simple-operating-system

commit 4a76cddb8ea1555e0b30cadcb837fb8fc1682260

services: hurd-vm: Support persistent images again.
Fixes a regression introduced in bab6434f5855b92631615fdd8a2d2a225da28359
whereby following the example from the manual regarding non-volatile images
would no longer work.

* gnu/services/virtualization.scm (hurd-vm-shepherd-service): Pass ‘image’ to
‘system-image’ only if it’s an image.
* doc/guix.texi (Virtualization Services): Explicitly say that the ‘image’
field can contain a file name.  Remove use of ‘const’ for the ‘image’ field in
persistent image example and adjust text.

Fixes: guix/guix#4130
Reported-by: Maxim Cournoyer <maxim@guixotic.coop>
Change-Id: I1a81ce27fb45978a681310b8a8788ab671b3edf8

doc/guix.texi
@@ -40720,6 +40720,10 @@ The image type name.  Use @code{'hurd-qcow2} for a 32-bit image or
 The image object representing the disk image of this virtual machine
 (@pxref{System Images}).
 
+Alternatively, this can be the file name (a string) of an image, which
+is useful if you want the image to persist across service restarts---see
+example below.
+
 @item @code{disk-size} (default: @code{'guess})
 The size of the disk image.
 
@@ -40818,15 +40822,16 @@ with the following non-volatile secrets, unless they already exist:
 @end table
 @end deftp
 
-Note that by default the VM image is volatile, i.e., once stopped the
-contents are lost.  If you want a stateful image instead, override the
-configuration's @code{image} and @code{options} without
-the @code{--snapshot} flag using something along these lines:
+Note that by default the VM image is volatile---i.e., once the service
+is stopped, the contents are lost.  If you want a persistent image
+instead, override the configuration's @code{image} and @code{options}
+without the @option{--snapshot} flag using something along these lines:
 
 @lisp
 (service hurd-vm-service-type
          (hurd-vm-configuration
-          (image   (const "/out/of/store/writable/hurd.img"))
+          ;; Specify an out-of-store, persistent image.
+          (image "/out/of/store/writable/hurd.img")
           (options '())))
 @end lisp
 

gnu/services/virtualization.scm
@@ -1897,7 +1897,9 @@ is added to the OS specified in CONFIG."
                       "-m" (number->string #$memory-size)
                       #$@net-options
                       #$@options
-                      "--hda" #+(system-image image)
+                      "--hda" #+(if (image? image)
+                                    (system-image image)
+                                    image)
 
                       ;; Cause the service to be respawned if the guest
                       ;; reboots (it can reboot for instance if it did not