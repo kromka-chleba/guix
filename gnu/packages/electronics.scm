7922c52fee8e25c8cf217a1f3fc41e9bf9d7ad92
Cayetano Santos <csantosb@inventati.org>
2025-11-15 17:31:34 +0100

    gnu: python-cocotb: Update to 2.0.1.
    * gnu/packages/electronics.scm (python-cocotb): Update to 2.0.1.
    
    Change-Id: Id69c69fbd5f702284115cbed67bd5a2033ea7066

--- gnu/packages/electronics.scm
+++ gnu/packages/electronics.scm
@@ -1616,7 +1616,7 @@ Standard} data mode.")
 (define-public python-cocotb
   (package
     (name "python-cocotb")
-    (version "2.0.0")
+    (version "2.0.1")
     (source
      (origin
        (method git-fetch)
@@ -1625,7 +1625,7 @@ Standard} data mode.")
               (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0b9wc14df11wkwa20wkij4iip07841qsr0yir9g7dww069rj36q6"))))
+        (base32 "0lyv0q1zqldrzfpyy3k1cxdnsw05gv73x5iid20yagvgb6l0sx1d"))))
     (build-system pyproject-build-system)
     (arguments
      (list
