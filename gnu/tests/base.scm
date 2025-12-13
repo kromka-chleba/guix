kromka-chleba/guix

commit 85ac164c41fc4c93d3cb2a5d3321c63598c2855f

services: nscd: Enable ‘passwd’ and ‘group’ caches by default.
This allows users to specify NSS plugins such as LDAP via the
‘name-services’ field of <nscd-configuration>.  Failing that, user code
will dlopen whatever passwd/group plugins are listed in
/etc/nsswitch.conf, which is likely to fail, typically because those are
not in $LD_LIBRARY_PATH.

* gnu/services/base.scm (%nscd-default-caches): Add ‘passwd’ and ‘group’
caches.

Change-Id: I9c03346a1de2710685f7801eccd2e08007427f5d

gnu/services/base.scm
@@ -1340,7 +1340,22 @@ the tty to run, among other things."
                     (positive-time-to-live (* 3600 24))
                     (negative-time-to-live 3600)
                     (check-files? #t)             ;check /etc/services changes
-                    (persistent? #t))))
+                    (persistent? #t))
+
+        ;; Enable minimal caching of the user databases, not so much for
+        ;; caching but rather to allow that uses of NSS plugins like LDAP
+        ;; don't lead user processes to dlopen them (which is likely to fail
+        ;; due to them not being found in $LD_LIBRARY_PATH).
+        (nscd-cache (database 'passwd)
+                    (positive-time-to-live 600)
+                    (negative-time-to-live 20)
+                    (check-files? #t)             ;check /etc/passwd changes
+                    (persistent? #f))
+        (nscd-cache (database 'group)
+                    (positive-time-to-live 600)
+                    (negative-time-to-live 20)
+                    (check-files? #t)             ;check /etc/group changes
+                    (persistent? #f))))
 
 (define-deprecated %nscd-default-configuration
   #f

commit a3772f7bb58ca90a5dc374014fe3b92c8f34ae26

services: nscd: Cause PID 1 to drop nscd database mappings on shutdown.
Partly fixes guix/guix#4269.

Fixes a bug whereby shepherd (PID 1) could retain memory mappings for
/var/run/nscd/dbXXX, which are created by glibc’s NSS from database file
descriptors sent by nscd.  Those mappings could then prevent
‘root-file-system’ from re-mounting the root file system as read-write.
This change causes PID 1 to drop these mappings.

PID 1 typically calls libc database functions such as ‘getgr’ when dealing
with AF_UNIX endpoints for socket-activated services, to look up the socket’s
owner and group.  This is where the bug would manifest.

The regression may have been introduced by
85ac164c41fc4c93d3cb2a5d3321c63598c2855f, which caused nscd to handle the
password and group databases.

* gnu/services/base.scm (nscd-shepherd-service): In ‘stop’ procedure, call
‘getpw’, ‘getgr’, and ‘getaddrinfo’.
* gnu/tests/base.scm (run-root-unmount-test) <"open libc NSS database">: New
test.
(%test-root-unmount): Add #:imported-modules.

Change-Id: I197cc8c82165c631f857415898137412ce9bd439
Reported-by: Rutherther <rutherther@ditigal.xyz>

gnu/services/base.scm
@@ -1557,7 +1557,21 @@ the tty to run, among other things."
                                                   (string-append dir "/lib"))
                                                 (list #$@name-services))
                                            ":")))))
-           (stop #~(make-kill-destructor))
+           (stop #~(let ((terminate (make-kill-destructor)))
+                     (lambda (process)
+                       (terminate process)
+
+                       ;; PID 1 might have mapped nscd database files via
+                       ;; '__nscd_get_mapping'.  Call the relevant libc
+                       ;; functions (those with a corresponding GETFD* request
+                       ;; type) to cause PID 1 to notice that those mappings
+                       ;; are stale and to unmap them.  Failure to do so would
+                       ;; prevent the root file system from being remounted
+                       ;; read-only when shutting down.
+                       (false-if-exception (getpw "root"))
+                       (false-if-exception (getgr "root"))
+                       (false-if-exception (getaddrinfo "localhost" "http"))
+                       #f)))
            (modules `((ice-9 popen)               ;for the actions
                       (ice-9 rdelim)
                       (ice-9 match)

gnu/tests/base.scm
@@ -827,6 +827,18 @@ in a loop.  See <http://bugs.gnu.org/26931>.")
                                                      ,witness-size))))))
                                marionette))
 
+            ;; Cause PID 1 to create a mapping to nscd's database files.
+            ;; Those mappings used to prevent 'root-file-system' to remount
+            ;; read-only on shutdown.  See
+            ;; <https://codeberg.org/guix/guix/issues/4269>.
+            (test-equal "open libc NSS database"
+              "root"
+              (marionette-eval '(begin
+                                  (use-modules (gnu services herd))
+                                  (start-service 'nscd) ;wait for nscd
+                                  (eval-there '(passwd:name (getgr "root"))))
+                               marionette))
+
             ;; Halt the system.
             (marionette-eval '(system* "/run/current-system/profile/sbin/halt")
                              marionette)
@@ -891,7 +903,10 @@ in a loop.  See <http://bugs.gnu.org/26931>.")
     "Make sure the root file system is cleanly unmounted when the system is
 halted.")
    (value
-    (let ((os (marionette-operating-system %simple-os)))
+    (let ((os (marionette-operating-system
+               %simple-os
+               #:imported-modules '((gnu services herd)
+                                    (guix combinators)))))
       (run-root-unmount-test os)))))
 
 