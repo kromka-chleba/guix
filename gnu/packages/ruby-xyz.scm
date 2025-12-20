12da75fa7459f64acb158110b7af4df9089df5d5
Yan Abu Arab <yanabuarab@gmail.com>
2025-12-20 02:25:58 +0200

    gnu: Add ruby-tty-screen.
    * gnu/packages/ruby-xyz.scm (ruby-tty-screen): New variable.
    
    Change-Id: I95dbd3b246bdfaeaf405b801ecee8ea601181d53

--- gnu/packages/ruby-xyz.scm
+++ gnu/packages/ruby-xyz.scm
@@ -563,6 +563,34 @@ files in a directory.")
 hosts by wrapping the @file{rsync} binary.")
     (license license:expat)))
 
+(define-public ruby-tty-screen
+  (package
+    (name "ruby-tty-screen")
+    (version "0.8.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (rubygems-uri "tty-screen" version))
+       (sha256
+        (base32 "0l4vh6g333jxm9lakilkva2gn17j6gb052626r1pdbmy2lhnb460"))))
+    (build-system ruby-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'check
+            (lambda _
+              (invoke "rspec" "-c" "spec"))))))
+    (native-inputs (list ruby-rspec))
+    (synopsis
+     "Terminal screen detection")
+    (description
+     "Terminal screen size detection that works on Linux, @code{macOS} and
+Windows systems and supports Ruby MRI, JRuby, @code{TruffleRuby} and Rubinius
+interpreters.")
+    (home-page "https://ttytoolkit.org")
+    (license license:expat)))
+
 (define-public ruby-i18n
   (package
     (name "ruby-i18n")
