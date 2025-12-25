2044627121ede87241558aa7027c89e52bb1e226
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-25 18:51:56 +0100

    gnu: gdmd: Alias to gdmd-14.
    * gnu/packages/gcc.scm (gdmd): Redefine as an alias to gdmd-14.  Avoids
    ambiguous package specification warnings and executing make-gdmd twice.
    
    Change-Id: I45455d4b3f547d8d77bc46a553102eebf727b45e

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -1613,8 +1613,7 @@ also includes the druntime and phobos libraries."
 (define-public gdmd-15
   (make-gdmd gdc-15))
 
-(define-public gdmd
-  (make-gdmd gdc))
+(define-public gdmd gdmd-14)
 
 (define-public gm2
   (hidden-package
