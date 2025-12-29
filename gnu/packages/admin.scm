b452f48ac5d4c12b06c9b38f9b21121ba7160b30
ROCKTAKEY <rocktakey@gmail.com>
2025-12-30 01:03:48 +0900

    gnu: Add witr.
    Change-Id: I681e0b12f8b132b647685507c97c85207402513b

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -78,6 +78,7 @@
 ;;; Copyright © 2024 nik gaffney <nik@fo.am>
 ;;; Copyright © 2025 Simon Streit <simon@netpanic.org>
 ;;; Copyright © 2025 Luca Kredel <luca.kredel@web.de>
+;;; Copyright © 2025 ROCKTAKEY <rocktakey@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -5928,6 +5929,40 @@ setup, maintenance, supervision, or any long-running processes.")
       (description "Utility to convert @code{lsof} output to a graph showing
 FIFO and UNIX interprocess communication.")
       (license license:bsd-2))))
+
+(define-public witr
+  (package
+    (name "witr")
+    (version "0.1.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/pranshuparmar/witr")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "016yiwn0b00ynllka6h2w6gk2yjzj5nx2xf26shb0cz4crdwww0d"))))
+    (build-system go-build-system)
+    (arguments
+     `(#:import-path "github.com/pranshuparmar/witr/cmd/witr"
+       #:unpack-path "github.com/pranshuparmar/witr"
+       #:install-source? #f
+       #:build-flags (list "-ldflags"
+                           ,(string-append "-X main.version=" version))))
+    (home-page "https://github.com/pranshuparmar/witr")
+    (synopsis "Utility to show why the process is running")
+    (description
+     "The @dfn{witr} (why-is-this-running), is a utility to show:
+@enumerate
+@item What is running?
+@item How did it start?
+@item What is keeping it running?
+@item What context does it belong to?
+@end enumerate
+It shows which process uses the port, and its information like process tree,
+working directory, user, pid, command, and so on.")
+    (license license:asl2.0)))
 
 (define-public runitor
   (package
