9cf18ee8d839b8ba59f86c43446ee1f0a2211be4
Patrick Norton <patrick.147.norton@gmail.com>
2026-01-04 11:46:01 -0500

    gnu: Add go-github-com-grpc-ecosystem-go-grpc-middleware-providers-prometheus.
    * gnu/packages/prometheus.scm
    (go-github-com-grpc-ecosystem-go-grpc-middleware-providers-prometheus):
    New variable.
    
    Change-Id: Ib7532a542cc7bd603b084d9f66fdbd13131a8a13

--- gnu/packages/prometheus.scm
+++ gnu/packages/prometheus.scm
@@ -114,6 +114,53 @@ exporting @code{OpenCensus} views as Prometheus metrics.")
 @url{https://github.com/grpc/grpc-go, gRPC Go} servers and clients.")
     (license license:asl2.0)))
 
+(define-public go-github-com-grpc-ecosystem-go-grpc-middleware-providers-prometheus
+  (package
+    (name
+     "go-github-com-grpc-ecosystem-go-grpc-middleware-providers-prometheus")
+    (version "1.1.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/grpc-ecosystem/go-grpc-middleware")
+             (commit (go-version->git-ref version
+                                          #:subdir "providers/prometheus"))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0fr8z4dr9n1x6zgs2n7m2wd1j2wnnmig0xq099xcg5lvcxiqjv73"))
+       (modules '((guix build utils)
+                  (ice-9 ftw)
+                  (srfi srfi-26)))
+       (snippet #~(begin
+                    (define (delete-all-but directory . preserve)
+                      (with-directory-excursion directory
+                        (let* ((pred (negate (cut member <>
+                                                  (cons* "." ".." preserve))))
+                               (items (scandir "." pred)))
+                          (for-each (cut delete-file-recursively <>) items))))
+                    (delete-all-but "." "providers")
+                    (delete-all-but "providers" "prometheus")))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path
+      "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+      #:unpack-path "github.com/grpc-ecosystem/go-grpc-middleware"))
+    (propagated-inputs (list
+                        go-github-com-grpc-ecosystem-go-grpc-middleware-v2
+                        go-github-com-prometheus-client-golang
+                        go-github-com-prometheus-client-model
+                        go-github-com-stretchr-testify
+                        go-google-golang-org-grpc))
+    (home-page "https://github.com/grpc-ecosystem/go-grpc-middleware")
+    (synopsis "GRPC middleware for Prometheus")
+    (description
+     "Package prometheus provides a standalone interceptor for metrics.  It's next
+iteration of deprecated
+@url{https://github.com/grpc-ecosystem/go-grpc-prometheus}.")
+    (license license:asl2.0)))
+
 (define-public go-github-com-mwitkow-go-conntrack
   (package
     (name "go-github-com-mwitkow-go-conntrack")
