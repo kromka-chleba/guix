d35036be2edb0d5183c4101779f2351877fe96da
Ethan Blanton <elb@lostbits.net>
2026-01-05 12:37:02 -0500

    gnu: go-github-com-go-openapi-runtime: Remove dependency on go-1.23
    * gnu: gnu/packages/golang-web.scm (go-github-com-go-openapi-runtime):
    Add -vet=off to test arguments so that make check works with go 1.24+
    
    Change-Id: I30d95ab876b5c1d75f6229d97c8e0ccea1b3ee4f

--- gnu/packages/golang-web.scm
+++ gnu/packages/golang-web.scm
@@ -5208,9 +5208,9 @@ Supports JSON and YAML documents.")
     (build-system go-build-system)
     (arguments
      (list
-      #:go go-1.23
       #:embed-files #~(list "jsonschema-draft-04\\.json" "schema\\.json")
-      #:import-path "github.com/go-openapi/runtime"))
+      #:import-path "github.com/go-openapi/runtime"
+      #:test-flags #~(list "-vet=off")))
     (native-inputs
      (list go-github-com-stretchr-testify))
     (propagated-inputs
