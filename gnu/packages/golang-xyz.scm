41977a750b670d3c0c277b2eff986814d816540f
Roman Scherer <roman@burningswell.com>
2026-01-10 16:20:19 +0100

    gnu: go-github-com-charmbracelet-x-input: Use git-fetch/lfs.
    This repository uses Git LFS for binary files, requiring git-fetch/lfs
    to properly fetch the content on systems where git-lfs is not available
    to the daemon's builtin:git-download builder.
    
    * gnu/packages/golang-xyz.scm (go-github-com-charmbracelet-x-input): Use
    git-fetch/lfs method.
    
    Change-Id: I570d2c43188afecfee9323ede00523724a741e41

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -3998,7 +3998,7 @@ package provides an API for comparing Golden files.")
     (version "0.3.7")
     (source
      (origin
-       (method git-fetch)
+       (method git-fetch/lfs)
        (uri (git-reference
               (url "https://github.com/charmbracelet/x")
               (commit (go-version->git-ref version
