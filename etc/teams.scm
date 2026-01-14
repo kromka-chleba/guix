2e6a5a925be9b00fe9abd8589f054edc5541b108
Nguyễn Gia Phong <cnx@loang.net>
2026-01-14 17:57:19 +0900

    teams: Add cnx to team sysadmin.
    * etc/teams.scm: Make cnx a member of team sysadmin.
    
    Change-Id: Ifd707911ebfcfb550aee3d8e8d9ace27f5386a6e

--- etc/teams.scm
+++ etc/teams.scm
@@ -1403,7 +1403,7 @@ the \"texlive\" importer."
 (define-member (person "Nguyễn Gia Phong"
                        "cnx@loang.net"
                        "cnx")
-  python)
+  python sysadmin)
 
 (define-member (person "Lilah Tascheter"
                        "lilah@lunabee.space"
