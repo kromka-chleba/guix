63cff5c30c20f317d340a5e53875a8f629cde66e
Thanos Apollo <public@thanosapollo.org>
2026-01-21 08:38:06 +0200

    services: git-http-nginx-location-configuration: Fix uri-path issue.
    - gnu/services/version-control.scm (git-http-nginx-location-configuration):
    Replace dynamic uri-path-based regex with fixed pattern matching only git
    operations (info/refs, git-upload-pack, git-receive-pack).
    
    Change-Id: I9d3d0eaf78d96bc9584bc6080d9f4132fb4849a4

--- gnu/services/version-control.scm
+++ gnu/services/version-control.scm
@@ -235,7 +235,11 @@ access to exported repositories under @file{/srv/git}."
     (($ <git-http-configuration> package git-root export-all?
                                  uri-path fcgiwrap-socket)
      (nginx-location-configuration
-      (uri (string-append "~ /" (string-trim-both uri-path #\/) "(/.*)"))
+      (uri (string-append "~ "
+			  (if (string=? uri-path "/")
+                              ""
+                              (string-append "/" (string-trim-both uri-path #\/)))
+			  "/(info/refs|git-upload-pack|git-receive-pack)"))
       (body
        (list
         (list "fastcgi_pass " fcgiwrap-socket ";")
@@ -254,7 +258,7 @@ access to exported repositories under @file{/srv/git}."
               (plain-file "gitconfig"
                           (string-append "[safe]\n\tdirectory = " git-root "/*\n"))
               ";")
-        "fastcgi_param PATH_INFO $1;"))))))
+        "fastcgi_param PATH_INFO $uri;"))))))
 
 
 ;;;
