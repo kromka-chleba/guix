229b8ea390be9aff40e18b8f615ca3c71ac04c5d
Danny Milosavljevic <dannym@friendly-machines.com>
2026-01-25 15:53:16 +0100

    gnu: Add rust-codex-execpolicy-0.0.0.4c3d2a5.
    * gnu/packages/rust-crates.scm (rust-codex-execpolicy-0.0.0.4c3d2a5): New variable.
    
    Change-Id: Ibabd555c2bb3d9eac23c2db834f76883dd40227e

--- gnu/packages/rust-crates.scm
+++ gnu/packages/rust-crates.scm
@@ -3371,6 +3371,9 @@
 (define rust-codex-core-0.0.0.4c3d2a5
   package:rust-codex-0.0.0.4c3d2a5)
 
+(define rust-codex-execpolicy-0.0.0.4c3d2a5
+  package:rust-codex-0.0.0.4c3d2a5)
+
 (define rust-codspeed-2.10.0
   (crate-source "codspeed" "2.10.0"
                 "1x9anwlfzlfby794d5fcvp214bj8bp29br8pkksxwb7834djja6j"))
