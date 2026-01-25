87a74f1f6ebec71180abb905d2c9a07db9eaa2b5
Danny Milosavljevic <dannym@friendly-machines.com>
2026-01-25 15:53:17 +0100

    gnu: Add rust-codex-keyring-store-0.0.0.4c3d2a5.
    * gnu/packages/rust-crates.scm (rust-codex-keyring-store-0.0.0.4c3d2a5): New variable.
    
    Change-Id: I7156871ab0fbd0f5e62a27a95263ba39ba3d273b

--- gnu/packages/rust-crates.scm
+++ gnu/packages/rust-crates.scm
@@ -3380,6 +3380,9 @@
 (define rust-codex-git-0.0.0.4c3d2a5
   package:rust-codex-0.0.0.4c3d2a5)
 
+(define rust-codex-keyring-store-0.0.0.4c3d2a5
+  package:rust-codex-0.0.0.4c3d2a5)
+
 (define rust-codspeed-2.10.0
   (crate-source "codspeed" "2.10.0"
                 "1x9anwlfzlfby794d5fcvp214bj8bp29br8pkksxwb7834djja6j"))
