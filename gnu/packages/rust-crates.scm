47dadb47137e97a50309a08c0ff9d7f1fcd2f515
Danny Milosavljevic <dannym@friendly-machines.com>
2026-01-25 15:53:19 +0100

    gnu: Add rust-codex-rmcp-client-0.0.0.4c3d2a5.
    * gnu/packages/rust-crates.scm (rust-codex-rmcp-client-0.0.0.4c3d2a5): New variable.
    
    Change-Id: I55cb98436d8525cad2f55e2411fa4c23d47f60bb

--- gnu/packages/rust-crates.scm
+++ gnu/packages/rust-crates.scm
@@ -3395,6 +3395,9 @@
 (define rust-codex-protocol-0.0.0.4c3d2a5
   package:rust-codex-0.0.0.4c3d2a5)
 
+(define rust-codex-rmcp-client-0.0.0.4c3d2a5
+  package:rust-codex-0.0.0.4c3d2a5)
+
 (define rust-codspeed-2.10.0
   (crate-source "codspeed" "2.10.0"
                 "1x9anwlfzlfby794d5fcvp214bj8bp29br8pkksxwb7834djja6j"))
