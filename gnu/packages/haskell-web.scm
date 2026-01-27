793f5fad5c827306e0ff6f5a51f1297247a7c583
emma thompson <bigbookofbug@proton.me>
2026-01-26 18:18:48 -0700

    gnu: Add ghc-lens-aeson
    * gnu/packages/haskell-web.scm (ghc-lens-aeson): New variable.

--- gnu/packages/haskell-web.scm
+++ gnu/packages/haskell-web.scm
@@ -13,6 +13,7 @@
 ;;; Copyright © 2020 Alexandru-Sergiu Marton <brown121407@member.fsf.org>
 ;;; Copyright © 2020 Giacomo Leidi <therewasa@fishinthecalculator.me>
 ;;; Copyright © 2022 Alice Brenon <alice.brenon@ens-lyon.fr>
+;;; Copyright © 2026 bigbookofbug <bigbookofbug@proton.me>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -1202,6 +1203,32 @@ of a JSON value into a @code{Data.Aeson.Value}.")
 good error messages when parsing fails.  See also
 @url{http://harry.garrood.me/blog/aeson-better-errors/}.")
     (license license:expat)))
+
+(define-public ghc-lens-aeson
+  (package
+   (name "ghc-lens-aeson")
+   (version "1.2.3")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (hackage-uri "lens-aeson" version))
+     (sha256
+      (base32 "00ac8anw6a3alwlqqvbr1vp7brajrdp66ximl7ylvj28wbznmg3v"))))
+   (build-system haskell-build-system)
+   (properties '((upstream-name . "lens-aeson")))
+   (inputs (list ghc-lens
+		 ghc-text-short
+		 ghc-vector
+		 ghc-unordered-containers
+		 ghc-aeson
+		 ghc-scientific))
+   (arguments
+    `(#:cabal-revision ("2"
+			"1mw2ijrdkkpazgnfb1msahgf1dlygrcl2i3hi4g7vqf8b95knwss")))
+   (home-page "http://github.com/lens/lens-aeson/")
+   (synopsis "Law-abiding lenses for aeson")
+   (description "Law-abiding lenses for aeson.")
+   (license license:expat)))
 
 (define-public ghc-multipart
   (package
